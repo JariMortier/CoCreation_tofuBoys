605b8ddc1cd208220abab45640f3baaff8a2435a
jarmorti <Jari.Mortier@UGent.be>
2024-03-12 18:10:11 +0100

    edits the .ino file directly

--- turn_signal_16/img_decoder/img_manip.py
+++ turn_signal_16/img_decoder/img_manip.py
@@ -13,19 +13,40 @@ r_values = []
 g_values = []
 b_values = []
 
-#Fit values to curve to weight
-weight = 3
-
 for y in range(height):
     for x in range(width):
         r, g, b = pixels[x, y]
-        r_values.append(str(floor(r ** (weight)/255 ** (weight - 1))))
-        g_values.append(str(floor(g ** (weight)/255 ** (weight - 1))))
-        b_values.append(str(floor(b ** (weight)/255 ** (weight - 1))))
+        r_values.append(str(r))
+        g_values.append(str(g))
+        b_values.append(str(b))
 r_string = "{" + ", ".join(r_values) + "}"
 g_string = "{" + ", ".join(g_values) + "}"
 b_string = "{" + ", ".join(b_values) + "}"
 
-with open("rgb.txt", "w") as text:
-    text.write(f"uint8_t rood[] = {r_string};\nuint8_t groen[] = {g_string};\nuint8_t blauw[] = {b_string};")
+with open("matrix_pics_backup\matrix_pics_backup.ino", "r") as text:
+    lines = text.readlines()
+
+with open("matrix_pics_backup\matrix_pics_backup.ino", "w") as text:
+    new_text = ""
+    for line in lines:
+        #print(line)
+        if "uint8_t rood[] = " in line:
+            new_text += f"uint8_t rood[] = {r_string};\n"
+            print("Found reds")
+
+        elif "uint8_t groen[] = " in line:
+            new_text += f"uint8_t groen[] = {g_string};\n"
+            print("Found greens")
+
+        elif "uint8_t blauw[] = " in line:
+            new_text += f"uint8_t blauw[] = {b_string};\n"
+            print("Found blues")
+        
+        else:
+            new_text += line
+    text.write(new_text)
+
+
+#with open("rgb.txt", "w") as text:
+#    text.write(f"uint8_t rood[] = {r_string};\nuint8_t groen[] = {g_string};\nuint8_t blauw[] = {b_string};")
 print("done!")
