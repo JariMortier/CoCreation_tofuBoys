790788b839612664f665545acbda9084af9efa96
jarmorti <Jari.Mortier@UGent.be>
2024-03-12 11:07:19 +0100

    custom pictures through RGB values

---
+++ turn_signal_16/img_decoder/img_manip.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from math import *
+
+# Open the fucking image
+image = Image.open(r"turn_signal_16\img_decoder\mario.png")
+
+# Get data
+pixels = image.load()
+
+# Get RGB values
+width, height = image.size
+r_values = []
+g_values = []
+b_values = []
+
+#Fit values to curve to weight
+weight = 3
+
+for y in range(height):
+    for x in range(width):
+        r, g, b = pixels[x, y]
+        r_values.append(str(floor(r ** (weight)/255 ** (weight - 1))))
+        g_values.append(str(floor(g ** (weight)/255 ** (weight - 1))))
+        b_values.append(str(floor(b ** (weight)/255 ** (weight - 1))))
+r_string = "{" + ", ".join(r_values) + "}"
+g_string = "{" + ", ".join(g_values) + "}"
+b_string = "{" + ", ".join(b_values) + "}"
+
+with open("rgb.txt", "w") as text:
+    text.write(f"Reds: {r_string} \nGreens: {g_string} \nBlues: {b_string}")
+print("done!")
